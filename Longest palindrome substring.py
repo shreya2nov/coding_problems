a6dfc969c95b9e6e0b84526f88c736c711978f1b
shreya2nov <shrmittal29@gmail.com>
2021-05-19 20:33:17 +0530

    Create Longest palindrome substring.py

---
+++ Longest palindrome substring.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        
+        def helper(l,r):
+            while( l>=0 and r<len(s) and s[l]==s[r]):
+                l=l-1
+                r=r+1
+            return s[l+1:r]
+        
+        res=""
+        for i in range(len(s)):
+            test=helper(i,i)
+            if len(test)>len(res):
+                res=test
+            test=helper(i,i+1)
+            if len(test)>len(res):
+                res=test
+        return res
+            
