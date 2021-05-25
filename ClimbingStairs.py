d24b791f5fd7d4a42d1b819d62da09132772a776
shreya2nov <shrmittal29@gmail.com>
2021-05-25 21:32:28 +0530

    Create ClimbingStairs.py

---
+++ ClimbingStairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n==1:
+            return 1
+        first=1
+        second=2
+        
+        for i in range(3,n+1):
+            third=first+second
+            first=second
+            second=third
+            
+        return second
+       
+        
